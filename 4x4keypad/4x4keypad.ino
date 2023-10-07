cb99cc8b03a030a1256159bddbbf8f68a5c66cac
Leon Leibbrandt <leon@bluepenguin.co.za>
2023-10-07 15:37:00 +0200

    Cleanup

--- 4x4keypad/4x4keypad.ino
+++ 4x4keypad/4x4keypad.ino
@@ -1,119 +1,124 @@
-#include "Arduino.h"
-#include "stm32f1xx_hal.h"
+/*
+  Timebase callback
+  This example shows how to configure HardwareTimer to execute a callback with some parameter at regular interval.
+  Callback toggles pin.
+  Once configured, there is only CPU load for callbacks executions.
+*/
 
-#define ROW0_PIN PA0
-#define ROW1_PIN PA1
-#define ROW2_PIN PA2
-#define ROW3_PIN PA3
+#if !defined(STM32_CORE_VERSION) || (STM32_CORE_VERSION  < 0x01090000)
+#error "Due to API change, this sketch is compatible with STM32_CORE_VERSION  >= 0x02000000"
+#endif
 
-#define COL0_PIN PA4
-#define COL1_PIN PA5
-#define COL2_PIN PA6
-#define COL3_PIN PA7
+#include <U8x8lib.h>
 
-volatile uint8_t buttonState[4][4];
-volatile uint8_t lastButtonState[4][4];
+#include <array>
+#include <map>
 
-// Timer setup for row control
-TIM_HandleTypeDef htim2;
-void configureTimer() {
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  __HAL_RCC_TIM2_CLK_ENABLE();
+// const uint8_t rowPins[] = {PA0, PA1, PA2, PA3};
+std::array<uint32_t, 4> rowPins = {PA0, PA1, PA2, PA3};
+std::array<uint32_t, 4> colPins = {PA4, PA5, PA6, PA7};
+volatile uint8_t currentRowIndex = 0;
+volatile uint8_t buttonState[] = {0,0};
 
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 7200 - 1; // Adjust this value to change the timer frequency
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 10; // 10 ms
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  HAL_TIM_Base_Init(&htim2);
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig);
+std::size_t getPinIndex(uint32_t pinValue) {
+    for (std::size_t i = 0; i < colPins.size(); i++) {
+        if (colPins[i] == pinValue) {
+            return i; // Return the index when the pin is found
+        }
+    }
+    return colPins.size(); // Return a value outside the valid index range when not found
+}
 
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig);
+
+// Every second, print on serial MyData. And increment it.
+void Update_IT_callback()
+{
+  // Serial.println(*data);
+  // *data = *data + 1;
+  digitalWrite(rowPins.at(currentRowIndex), LOW);
+  currentRowIndex = (currentRowIndex + 1) % rowPins.size();
+  digitalWrite(rowPins.at(currentRowIndex), HIGH);
+  attachColumnInterrupts();
 }
 
 void configureRowPins() {
-  pinMode(ROW0_PIN, OUTPUT);
-  pinMode(ROW1_PIN, OUTPUT);
-  pinMode(ROW2_PIN, OUTPUT);
-  pinMode(ROW3_PIN, OUTPUT);
-}
-
-void setupColumnInterrupts() {
-  attachInterrupt(digitalPinToInterrupt(COL0_PIN), handleButtonInterrupt, FALLING);
-  attachInterrupt(digitalPinToInterrupt(COL1_PIN), handleButtonInterrupt, FALLING);
-  attachInterrupt(digitalPinToInterrupt(COL2_PIN), handleButtonInterrupt, FALLING);
-  attachInterrupt(digitalPinToInterrupt(COL3_PIN), handleButtonInterrupt, FALLING);
-}
-
-void setup() {
-  HAL_Init();
-  configureTimer();
-  configureRowPins();
-
-  // Initialize button states
-  for (int row = 0; row < 4; row++) {
-    for (int col = 0; col < 4; col++) {
-      buttonState[row][col] = HIGH; // Initialize to HIGH (released)
-    }
+  for (std::size_t i = 0; i < rowPins.size(); i++) {
+    pinMode(rowPins[i], OUTPUT);
   }
-
-  setupColumnInterrupts();
-  HAL_TIM_Base_Start(&htim2);
 }
 
-void loop() {
-  // No code should be in the loop function as requested.
+void configureColumnPins() {
+  for (std::size_t i = 0; i < colPins.size(); i++) {
+    pinMode(colPins[i], INPUT_PULLDOWN);
+  }
 }
 
-void handleButtonInterrupt() {
- // Button interrupt callback
+void attachColumnInterrupts() {
+  for (std::size_t col = 0; col < colPins.size(); col++) {
+      attachInterrupt(digitalPinToInterrupt(colPins[col]), std::bind(handleButtonInterrupt, &colPins[col]), FALLING);
+  }
+}
+void detachColumnInterrupts() {
+  for (std::size_t col = 0; col < colPins.size(); col++) {
+      detachInterrupt(digitalPinToInterrupt(colPins[col]));
+  }
+}
+void setup()
+{
+//  Serial.begin(9600); 
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_px437wyse700a_2x2_r);
+
+#if defined(TIM1)
+  TIM_TypeDef *Instance = TIM1;
+#else
+  TIM_TypeDef *Instance = TIM2;
+#endif
+
+  // Instantiate HardwareTimer object. Thanks to 'new' instanciation, HardwareTimer is not destructed when setup() function is finished.
+  HardwareTimer *MyTim = new HardwareTimer(Instance);
+
+  // configure pin in output mode
+  configureRowPins();
+  configureColumnPins();
+  attachColumnInterrupts();
+
+  MyTim->setOverflow(10, MICROSEC_FORMAT); // 100 Hz
+  MyTim->attachInterrupt(Update_IT_callback);
+  MyTim->resume();
+  
+  // u8x8.setBusClock(1);
+  // u8x8.begin();
+  // u8x8.setFont(u8x8_font_px437wyse700a_2x2_r);
+  // u8x8.drawString(0, 20, "Starting");
+}
+
+void handleButtonInterrupt(uint32_t* data) {
+  // Button interrupt callback
   // Handle button presses and releases here
   // Debounce by checking the button state after a short delay
 
   // Determine which column triggered the interrupt
-  uint8_t col = 0;
-  if (digitalRead(COL0_PIN) == LOW) col = 0;
-  else if (digitalRead(COL1_PIN) == LOW) col = 1;
-  else if (digitalRead(COL2_PIN) == LOW) col = 2;
-  else if (digitalRead(COL3_PIN) == LOW) col = 3;
+  uint8_t col = getPinIndex(*data);
+  uint8_t row = currentRowIndex;
+  detachColumnInterrupts();
+  buttonState[0] = row;
+  buttonState[1] = col;
+  updateDisplay();
+}
 
-  // Wait for debounce time
-  delay(10); // Adjust this value as needed for your debouncing requirements
+void updateDisplay() {
+  u8x8.drawString(0, 0, u8x8_u8toa(buttonState[0], 3));
+  u8x8.drawString(0, 20, u8x8_u8toa(buttonState[1], 3));
 
-  // Read the button state again after debounce
-  uint8_t state = digitalRead(COL0_PIN + col);
+  // u8x8.setFont(u8x8_font_px437wyse700b_2x2_r);
+  // u8x8.drawString(0, 2, "ABC defg");
+}
 
-  // Determine the row that corresponds to the interrupt source
-  uint8_t row = 0;
-  if (state == LOW) {
-    if (digitalRead(ROW0_PIN) == HIGH) row = 0;
-    else if (digitalRead(ROW1_PIN) == HIGH) row = 1;
-    else if (digitalRead(ROW2_PIN) == HIGH) row = 2;
-    else if (digitalRead(ROW3_PIN) == HIGH) row = 3;
-  }
-
-  // Update button state and lastButtonState
-  buttonState[row][col] = state;
-  lastButtonState[row][col] = state;
-
-  // Detach the interrupt for this column (button) until the next cycle
-  detachInterrupt(digitalPinToInterrupt(COL0_PIN + col));
-
-  // Reattach the interrupt for this column (button) after a delay (next cycle)
-  delay(10); // Adjust this value as needed for your debouncing requirements
-  attachInterrupt(digitalPinToInterrupt(COL0_PIN + col), handleButtonInterrupt, FALLING);}
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-  // Timer interrupt callback
-  // Rotate through the rows here
-  static uint8_t currentRow = 0;
-  digitalWrite(ROW0_PIN + currentRow, LOW);
-  currentRow = (currentRow + 1) % 4;
-  digitalWrite(ROW0_PIN + currentRow, HIGH);
+void loop()
+{
+ /* Nothing to do all is done by hardware. Even no interrupt required. */
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Arduino
+
+Tinkering with stm32 uc, stm32duino and Arduino IDE 2
+
